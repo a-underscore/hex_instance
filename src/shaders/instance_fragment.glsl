4c2cdec4df8b9d19227d27a1f010c5b64bd013bc
Brendan Bortz <brendansbortz@protonmail.com>
2023-02-03 12:02:16 -0500

    works

--- src/shaders/instance_fragment.glsl
+++ src/shaders/instance_fragment.glsl
@@ -11,13 +11,12 @@ precision mediump float;
 #endif
 
 in vec2 tex_pos;
+in vec4 tex_color;
 
 out vec4 frag_color;
 
 uniform sampler2D image;
 
-uniform vec4 color;
-
 void main(void) {
-	frag_color = texture(image, tex_pos) * color;
+	frag_color = texture(image, tex_pos) * tex_color;
 }
--- src/shaders/instance_vertex.glsl
+++ src/shaders/instance_vertex.glsl
@@ -4,8 +4,10 @@ in vec2 position;
 in vec2 uv;
 in float z;
 in mat3 transform;
+in vec4 color;
 
 out vec2 tex_pos;
+out vec4 tex_color;
 
 uniform mat3 camera_transform;
 uniform mat4 camera_view;
@@ -16,4 +18,5 @@ void main(void) {
         gl_Position = camera_view * vec4(vec3(pos, z), 1.0);
 
 	tex_pos = uv;
+	tex_color = color;
 }
